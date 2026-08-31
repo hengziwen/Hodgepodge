7c02de28f791b2322761b84535683a47123c149b
hengziwen <112922249+hengziwen@users.noreply.github.com>
2026-08-31 17:27:21 +0800

    update:清理无用文件

--- read_uasset.ps1
+++
@@ -1,9 +0,0 @@
-$path = "e:\Project\Git\Hodgepodge\Content\Main\Experiences\Exp_HodgeDefaultExperience.uasset"
-$b = [System.IO.File]::ReadAllBytes($path)
-$s = [System.Text.Encoding]::ASCII.GetString($b, 0, [Math]::Min(4000, $b.Length))
-$m = [regex]::Matches($s, '[ -~]{4,}')
-$i = 0
-foreach ($x in $m) {
-    $i++
-    if ($i -le 60) { Write-Output $x.Value }
-}
